da776b1f906cb3158cad0ef9f8b36f6fa80a27dc
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2015-02-21 17:19:16 -0500

    ui + addition works

--- app/src/main/java/com/amv/anthonychin/mobilevectorcalculator/amv.java
+++ app/src/main/java/com/amv/anthonychin/mobilevectorcalculator/amv.java
@@ -3,6 +3,7 @@ package com.amv.anthonychin.mobilevectorcalculator;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.hardware.display.DisplayManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,21 +12,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.content.Intent;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class amv extends Activity {
 
+    private EditText firstInputOne;
+    private EditText firstInputTwo;
+    private EditText secondInputOne;
+    private EditText secondInputTwo;
+    private EditText thirdInputOne;
+    private EditText thirdInputTwo;
+    private TextView textView;
+
+    private int typeOfComputation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_amv);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-    }
 
+        typeOfComputation = 1; // current default addition
+
+        firstInputOne = (EditText)findViewById(R.id.first_vector_1);
+        firstInputTwo = (EditText)findViewById(R.id.first_vector_2);
+        secondInputOne = (EditText)findViewById(R.id.second_vector_1);
+        secondInputTwo = (EditText)findViewById(R.id.second_vector_2);
+        thirdInputOne = (EditText)findViewById(R.id.third_vector_1);
+        thirdInputTwo = (EditText)findViewById(R.id.third_vector_2);
+        textView = (TextView)findViewById(R.id.compute);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,6 +90,72 @@ public class amv extends Activity {
         }
     }
 
+    public void toAdd (View view) {
+        thirdInputOne.setVisibility(View.VISIBLE);
+        thirdInputTwo.setVisibility(View.VISIBLE);
+    }
+
+    public void toDot (View view) {
+        thirdInputOne.setVisibility(View.GONE);
+        thirdInputTwo.setVisibility(View.GONE);
+    }
+
+    public void toScalar (View view) {
+        thirdInputOne.setVisibility(View.GONE);
+        thirdInputTwo.setVisibility(View.GONE);
+
+    }
+    public void computeValue (View view) {
+        add();
+    }
+
+
+
+
+    private void add() {
+        boolean ThreeVectorAddition = false;
+        Vector thirdVector = null;
+
+        //Check if any of the inputs are empty
+        if (!thirdInputOne.getText().toString().equals("") &&
+                !thirdInputTwo.getText().toString().equals("")) {
+            ThreeVectorAddition = true;
+
+            double thirdVectorX = Double.parseDouble(thirdInputOne.getText().toString());
+            double thirdVectorY = Double.parseDouble(thirdInputTwo.getText().toString());
+
+            thirdVector = new Vector(thirdVectorX, thirdVectorY);
+        }
+
+        if(firstInputOne.getText().toString().equals("") 		||
+                firstInputTwo.getText().toString().equals("")	||
+                secondInputOne.getText().toString().equals("")	||
+                secondInputTwo.getText().toString().equals("")  ) {
+
+            textView.setText("Error: Please enter a number");
+            textView.setVisibility(View.VISIBLE);
+
+        } else {
+            double firstVectorX = Double.parseDouble(firstInputOne.getText().toString());
+            double firstVectorY = Double.parseDouble(firstInputTwo.getText().toString());
+            double secondVectorX = Double.parseDouble(secondInputOne.getText().toString());
+            double secondVectorY = Double.parseDouble(secondInputTwo.getText().toString());
+
+            Vector firstVector = new Vector(firstVectorX, firstVectorY);
+            Vector secondVector = new Vector(secondVectorX, secondVectorY);
+            Vector sum = null;
+
+            if (ThreeVectorAddition) {
+                sum = firstVector.add(secondVector, thirdVector);
+            } else {
+                sum = firstVector.add(secondVector);
+            }
+
+            textView.setText(sum.getX() + "i + " + sum.getY() +"j");
+            textView.setVisibility(View.VISIBLE);
+
+        }
+    }
 //    public void sendMessage (View view) {
 //        Intent intent = new Intent(this, DisplayMessageActivity.class);
 //        EditText editText = (EditText) findViewById(R.id.edit_message);
